f6b9e7e1ce7283ca9ded7484fa76439076b1a7c8
gerashdo <gerardo.jimenezarguelles@gmail.com>
2023-06-22 15:30:20 -0600

    [IMP] estate: Set field attrs to model T#72976
    Field attributes are included in some fields to imoprove model
    fields behavior.
    
    Exercises references:
    https://www.odoo.com/documentation/16.0/developer/tutorials/getting_started/06_firstui.html#some-new-attributes
    https://www.odoo.com/documentation/16.0/developer/tutorials/getting_started/06_firstui.html#default-values

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -8,10 +8,12 @@ class EstateProperty(models.Model):
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date(help="Date from when it will be available.")
+    date_availability = fields.Date(
+        copy=False, default=fields.Date.add(fields.Date.today(), months=3), help="Date from when it will be available."
+    )
     expected_price = fields.Float(required=True, help="Price at which you wish to sell the property.")
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
