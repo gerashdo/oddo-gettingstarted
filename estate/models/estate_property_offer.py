2747200902612cd7ea6deb146134f7347559eaae
gerashdo <gerardo.jimenezarguelles@gmail.com>
2023-06-27 13:57:13 -0600

    [ADD] estate: Add computed fields and onchanges T#72976
    Computed fields and their methods are created to make it easier for
    the user to fill in the property and offer information.
    
    Excercise reference link:
    https://www.odoo.com/documentation/16.0/developer/tutorials/getting_started/09_compute_onchange.html

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class PropertyOffer(models.Model):
@@ -15,3 +15,16 @@ class PropertyOffer(models.Model):
     )
     partner_id = fields.Many2one("res.partner", required=True)
     property_id = fields.Many2one("estate.property", required=True)
+    validity = fields.Integer(default=7, string="Validity (days)")
+    date_deadline = fields.Date(compute="_compute_date_deadline", inverse="_inverse_validity", string="Deadline")
+
+    def _inverse_validity(self):
+        for record in self:
+            end_date = fields.Date.to_date(record.create_date) if record.create_date else fields.Date.today()
+            record.validity = (record.date_deadline - end_date).days
+
+    @api.depends("validity")
+    def _compute_date_deadline(self):
+        for record in self:
+            start_date = record.create_date if record.create_date else fields.Date.today()
+            record.date_deadline = fields.Date.add(start_date, days=record.validity)
