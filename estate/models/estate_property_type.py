f27bb2d9e0a72d3e175c475eac5c53afbf518d29
gerashdo <gerardo.jimenezarguelles@gmail.com>
2023-06-28 13:26:08 -0600

    [ADD] estate: Add inline list and statusbar state T#72976
    Add One2many field in property type to list the properties related to it
    in its form view. Also use statubar widget to show property state.
    
    Exercise reference link: https://www.odoo.com/documentation/16.0/developer/tutorials/getting_started/12_sprinkles.html#inline-views

--- estate/models/estate_property_type.py
+++ estate/models/estate_property_type.py
@@ -6,5 +6,6 @@ class PropertyType(models.Model):
     _description = "Type of a property"
 
     name = fields.Char()
+    property_ids = fields.One2many("estate.property", "property_type_id")
 
     _sql_constraints = [("name_unique", "UNIQUE(name)", "The name must be unique.")]
