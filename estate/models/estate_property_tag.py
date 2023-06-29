0c7371a4ab1efe80684ebcad52b665f8600bb1bd
gerashdo <gerardo.jimenezarguelles@gmail.com>
2023-06-29 10:04:16 -0600

    [IMP] estate: Enhance ui behavior T#72976
    Add widgets and field attrs in the views to show or hide elements,
    display colors, allow edition, search defaults and edit fields inline.
    
    Exercises reference link:
    https://www.odoo.com/documentation/16.0/developer/tutorials/getting_started/12_sprinkles.html#attributes-and-options

--- estate/models/estate_property_tag.py
+++ estate/models/estate_property_tag.py
@@ -7,5 +7,6 @@ class PropertyTag(models.Model):
     _order = "name"
 
     name = fields.Char(required=True)
+    color = fields.Integer()
 
     _sql_constraints = [("name_unique", "UNIQUE(name)", "The name must be unique.")]
