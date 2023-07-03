afb6fbff37622d6bd9baa5d8a5165bbf84e9a15a
gerashdo <gerardo.jimenezarguelles@gmail.com>
2023-07-03 13:11:19 -0600

    [ADD] estate: Add security groups T#72976
    Create Admin and Manager groups to grant access rights based on the
    different types of user.
    
    Exercise reference link:
    https://www.odoo.com/documentation/16.0/es/developer/tutorials/restrict_data_access.html#groups

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -8,6 +8,7 @@
     "data": [
         "security/estate_security.xml",
         "security/ir.model.access.csv",
+        "security/estate_security.xml",
         "views/estate_property_views.xml",
         "views/estate_property_offer_views.xml",
         "views/estate_property_type_views.xml",
