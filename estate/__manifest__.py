59c7ac77395793e16cbbbb3eddaffe62984720a3
gerashdo <gerardo.jimenezarguelles@gmail.com>
2023-07-20 11:53:46 -0600

    [IMP] estate: Change security files order T#72976
    The security file where groups are defined should be loaded before the
    ir.model.access.csv file, otherwise the groups will not be found and
    raise an error.

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -7,8 +7,8 @@
     ],
     "data": [
         "data/estate.property.type.csv",
-        "security/ir.model.access.csv",
         "security/estate_security.xml",
+        "security/ir.model.access.csv",
         "views/estate_property_views.xml",
         "views/estate_property_offer_views.xml",
         "views/estate_property_type_views.xml",
